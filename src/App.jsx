4e39f4acd352c8ec72e225e08cfd92ea3d960fc1
Amir Salehi <nik.amirsa@gmail.com>
2022-08-16 12:03:05 +0430

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./assets/styles/main.scss"
+import Header from "./components/layouts/Header"
 
 function App() {
-  return <div className="App">hi</div>
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>Snowa Colors</h1>
+      <p>The guideline of Snowa colors tonality</p>
+      <code>You can click on the color to see the details</code>
+    </div>
+  )
+}
