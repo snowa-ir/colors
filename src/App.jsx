26909a69aed7086ad43843e25a0fa0f1cec37ded
Amir Salehi <nik.amirsa@gmail.com>
2022-08-16 12:09:33 +0430

    improve style structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./assets/styles/main.scss"
+import Footer from "./components/layouts/Footer"
 import Header from "./components/layouts/Header"
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Footer />
     </div>
   )
 }
