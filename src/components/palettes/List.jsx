9fac42e62e319996b38606958af12fc9337f8e6e
Amir Salehi <nik.amirsa@gmail.com>
2022-08-17 09:52:13 +0430

    load context in palette list

--- src/components/palettes/List.jsx
+++ src/components/palettes/List.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react"
+import MainContext from "../../context/mainContext"
 import Item from "./Item"
 
 export default function List() {
+  const { colors } = useContext(MainContext)
+
   return (
     <div className="palettes palletes--circle">
-      <Item />
+      {
+        colors.map(item => <Item color={item.color} />)
+      }
     </div>
   )
 }
